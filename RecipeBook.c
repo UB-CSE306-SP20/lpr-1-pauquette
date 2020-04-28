fef58c2d93e8c2c515a812816aa2df216c98f147
Hollis M Pauquette <hollispa@timberlake.cse.buffalo.edu>
2020-04-28 14:31:33 -0400

    #2: wrote function stubs in RecipeBook.c

---
+++ RecipeBook.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "RecipeBook.h"
+
+struct Ingredient * newIngredient(char * name, int caloriesPerGram) {
+  // Define later
+  return NULL;
+}
+
+struct Pantry * newPantry() {
+  // Define later
+  return NULL;
+}
+
+struct Recipe * newRecipe(char * name, int servings) {
+  // Define later
+  return NULL;
+}
+
+struct Book * newBook() {
+  // Define later
+  return NULL;
+}
+
+void addRecipe(struct Book * book, struct Recipe * recipe) {
+  // Define later
+}
+
+void addIngredient(struct Recipe * recipe, struct Ingredient * ingredient, int quantity) {
+  // Define later
+}
+
+void storeIngredient(struct Pantry * pantry, struct Ingredient * ingredient, int quantity) {
+  // eEfine later
+}
+
+struct Book * canMakeAny(struct Pantry * pantry, struct Book * book) {
+  // Define later
+  return NULL;
+}
+
+struct Book * canMakeAll(struct Pantry * pantry, struct Book * book) {
+  // Define later
+  return NULL;
+}
+
+struct Book * withinCalorieLimit(struct Pantry * pantry, struct Book * book, int limit) {
+  // Define later
+  return NULL;
+}
+
+int caloriesPerServing(struct Recipe * recipe) {
+  // Define later
+  return 0;
+}
